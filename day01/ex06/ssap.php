23ed434822d4b98f94665538e4fc3a90f3991d07
ksuomala <ksuomala@student.hive.fi>
2020-11-10 14:33:06 +0200

    ssap.php

---
+++ day01/ex06/ssap.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?
+
+	for ($i=1; $i < $argc; $i++){
+		$keywords = preg_split("/[\s,]+/", trim($argv[$i]));
+		$merge = array_merge((array)$merge, (array)$keywords);
+	}
+	sort($merge);
+	print_r(implode("\n", $merge));
+?>
