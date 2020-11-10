69d9d79293b9c126cddcc3889a72867a33626bbc
ksuomala <ksuomala@student.hive.fi>
2020-11-10 11:13:17 +0200

    Checks if the input is a number, or even/odd

---
+++ day01/ex02/oddeven.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?PHP
+
+print "Enter a number: ";
+
+$line = trim(fgets(STDIN));
+
+if (is_numeric($line))
+{
+	if ($line % 2)
+		print "The number $line is odd\n";
+	else
+		print "The number $line is even\n";
+}
+else
+	print "$line is not a number\n"
+?>
