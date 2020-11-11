349b7c16c510a081440a19f51e55dd1d8c56b91c
ksuomala <ksuomala@student.hive.fi>
2020-11-11 13:45:56 +0200

    added error check and seperator in case of no spaces

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -1,9 +1,12 @@
 #!/usr/bin/php
 <?
+	if ($argc < 2)
+		exit();
 	for ($i=1; $i < $argc; $i++){
-		$keywords = preg_split("/[\s,]+/", trim($argv[$i]));
+		$keywords = preg_split("/ +/", trim($argv[$i]));
 		$merge = array_merge((array)$merge, (array)$keywords);
 	}
 	sort($merge);
 	print_r(implode("\n", $merge));
+	print "\n";
 ?>
--- day01/ex11/do_op2.php
+++ day01/ex11/do_op2.php
@@ -13,15 +13,16 @@ if ($argc != 2)
 	print "Incorrect Parameters\n";
 	exit();
 }
-$arr = preg_split("/[\s,]+/", trim($argv[1]));
-$op = $arr[1];
-if (!is_numeric($arr[0]) || !is_numeric($arr[2]) || !is_operator(trim($op)))
+$arr = preg_split("/([ +-\/\*%]+)/", trim($argv[1]), -1, PREG_SPLIT_DELIM_CAPTURE);
+print_r($arr);
+$op = trim($arr[1]);
+if (!is_numeric($arr[0]) || !is_numeric($arr[2]) || !is_operator(trim($op)) || count($arr) != 3)
 {
 	print "Syntax Error\n";
 	exit();
 }
-$a = $arr[0];
-$b = $arr[2];
+$a = trim($arr[0]);
+$b = trim($arr[2]);
 if ($op == '+')
 	print ($a + $b);
 if ($op == '-')
