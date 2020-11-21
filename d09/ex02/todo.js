bb76e907ae5e7f2ee240cf19674a49ceccc12667
ksuomala <ksuomala@student.hive.fi>
2020-11-21 18:18:30 +0200

    fixed undefined print when all cookies are deleted

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,11 +1,20 @@
 let cookies = document.cookie.split(';');
 let cookie_id = cookies.length + 1;
 let parent = document.getElementById('ft_list');
+modStyle(parent);
+
+function modStyle(parentNode) {
+	parentNode.parentNode.style.backgroundColor = 'grey';
+	parentNode.style.backgroundColor = 'red';
+}
+
 if (cookies)
 {
 	cookies.forEach(function(item){
 		let div = document.createElement('div');
 		let todo = item.split('=');
+		if (!todo[1])
+			return;
 		div.innerHTML = todo[1];
 		div.setAttribute('id', todo[0]);
 		div.setAttribute('onclick', 'delElem(id)');
@@ -26,11 +35,10 @@ function newTask()
 		div.setAttribute('id', cookie_id);
 		div.setAttribute('onclick', 'delElem(id)');
 		parent.insertBefore(div, parent.firstChild);
-		debugmsg = "Inserting a cookie. cookie_id =" + cookie_id;
-		console.log(debugmsg);
 		document.cookie = cookie_id + "=" + task;
 	}
 }
+
 function delElem(id)
 {
 	if (confirm('Are you sure you want to delete task ' + id))
